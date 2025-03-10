2a291167f38b42d3d695dc9776f235323ed57425
Lucian <lucian.l@qycocu.com>
2025-03-10 18:01:47 +0800

    feat: add base url for github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { resolve } from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/ExpressWays/',
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
